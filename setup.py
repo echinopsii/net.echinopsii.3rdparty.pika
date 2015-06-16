368c4075a8401e7b31f9a950690ecf163e847710
Mathilde Ffrench <mathilde.ffrench@echinopsii.net>
2015-06-16 15:49:57 +0200

    [ACC-159] change distrib name

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ long_description = ('This is a forked version of Pika which help you tune '
                     'primarily for use with RabbitMQ, but should also work '
                     'with other AMQP 0-9-1 brokers.')
 
-setup(name='epika',
+setup(name='epika-python2',
       version='0.9.14',
       description='echinopsii Pika Python AMQP Client Library',
       long_description=long_description,
